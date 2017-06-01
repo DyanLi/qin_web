f526b8937a1373a9b239845e424e77423ebd17de
DyanLi <danyang918@gmail.com>
2017-06-01 16:52:58 +0800

    active button

--- qinhuangdao/views.py
+++ qinhuangdao/views.py
@@ -7,11 +7,38 @@ def index(request):
     return render(request, "index.html")
 
 def detail(request,my_args):
-    post = Qinhuangdao.objects.all()[int(my_args)]
+    # my_args = page now
+
+    # if not my_args:
+    #     my_args = 1
+    # print(type(my_args))
+
+    try:
+        my_args = int(my_args)
+    except:
+        my_args = 1
+
+    if my_args < 1:
+        my_args = 1
+
+    start = 1
+    end  = Qinhuangdao.objects.all().__len__()
+
+    if my_args == start :
+        before_page = 1
+    else:
+        before_page = my_args - 1
+
+    if my_args > end-1:
+        next_page = my_args
+    else:
+        next_page = my_args +1
+
+    post = Qinhuangdao.objects.all()[my_args-1]
     # str = ("datetime = %s, d1 = %s, v1 = %s"
     #     % (post.datetime, post.direction1, post.velocity1))
     # return HttpResponse(str)
-    return render(request, "detail.html", {'post': post})
+    return render(request, "detail.html", {'post': post,'current_page':my_args,'before_page':before_page,'next_page':next_page,'end_page':end})
 
 def draw(request,my_args):
     post = Qinhuangdao.objects.all()[int(my_args)]
