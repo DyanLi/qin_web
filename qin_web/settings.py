c4fe70e77358f82d3f0412c4bda4aa3d638e57f0
DyanLi <danyang918@gmail.com>
2017-08-15 16:14:49 +0800

    Minor modifications of ui

--- qin_web/settings.py
+++ qin_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(r*qj&&j@xklxmm)c3hr0mthcb15#mz0vc&pj_%5hga5l5kpoy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,7 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
